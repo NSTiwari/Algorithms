5296bb95d869a8c93120813697a69f5f3f368b46
NSTiwari <tiwarinitin1999@gmail.com>
2021-02-22 00:25:57 +0530

    Dynamic list input.

---
+++ Short Code Snippets/input_list.py
@@ -0,0 +1,11 @@
+l1 = []
+
+# Take input.
+n = int(input("Enter the no. of elements: "))
+print()
+for i in range(n):
+	element = input("Enter element"+str(i+1)+": ")
+	l1.append(element)
+
+print()
+print(l1)
