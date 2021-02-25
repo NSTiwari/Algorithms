f7bc200854351cbb8ad970572aac89fc8f49b641
NSTiwari <tiwarinitin1999@gmail.com>
2021-02-25 21:04:30 +0530

    Mapper file for word count.

--- Short Code Snippets/mapper.py
+++ Short Code Snippets/mapper.py
@@ -15,5 +15,5 @@ for line in sys.stdin:
 	for word in words: 
 		# write the results to STDOUT (standard output); 
 		# what we output here will be the input for the 
-		# Reduce step, i.e. the input for reducer.py 
+		# Reduce step, i.e., the input for reducer.py 
 		print("%s\t%s" % (word, 1)) 
